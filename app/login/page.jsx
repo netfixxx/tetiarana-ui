349802a6a85df3cec8c6ef987634f27f2cb82216
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-17 16:07:59 +0300

    chore: add axios

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,36 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
 export default function Login() {
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const userCredentials = {
+        email,
+        password,
+        key: adminKey
+      };
+      axios.post('http://localhost:4000/login', userCredentials)
+        .then(response => {
+          if(response.status === 200 && response.data.status) {
+              navigate('/profil');
+          } else {
+              setErrorMessage('Email, mot de passe ou clé d\'administrateur incorrecte.');
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          setErrorMessage('Une erreur est survenue lors de la connexion.')
+        })
+    }
     return (
       <main className="flex min-h-screen items-center justify-center bg-gray-100">
         <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
           <h1 className="text-2xl font-bold text-center mb-4">Se connecter</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
                 Email
@@ -14,6 +41,7 @@ export default function Login() {
                 className="input input-bordered w-full"
                 placeholder="Entrez votre email"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mb-6">
@@ -26,6 +54,7 @@ export default function Login() {
                 className="input input-bordered w-full"
                 placeholder="Entrez votre mot de passe"
                 required
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="flex items-center justify-between">
