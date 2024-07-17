1e23b2b77a16db6dbd051d026b10b0f92d633642
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-17 10:14:13 +0300

    feat: navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,10 @@
-import Link from 'next/link';
 import './globals.css';
+import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Bienvenue sur votre nouveau projet Next.js</h1>
-        <p className="mb-8">Ceci est une page d'accueil simplifiée utilisant DaisyUI.</p>
-        <button className="btn btn-primary mb-4">Bouton Principal</button>
-        <button className="btn btn-secondary mb-4">Bouton Secondaire</button>
-        <button className="btn btn-accent mb-4">Bouton Accent</button>
-        <Link href="/login" legacyBehavior>
-          <a className="btn btn-primary">Se connecter</a>
-        </Link>
-      </div>
-    </main>
+    <div className="w-full max-w-7xl mx-auto fixed top-4 left-1/2 transform -translate-x-1/2 bg-transparent dark:bg-gray-900 rounded-lg shadow-lg border border-gray-100 dark:border-gray-800">
+      <Navbar/>
+    </div>
   );
 }
